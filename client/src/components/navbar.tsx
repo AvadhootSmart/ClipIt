aab285b494df541d7d8fa8e2414c5b415e0c2a3a
AvadhootSmart <1234avadhootsmart@gmail.com>
2025-05-28 20:29:35 +0530

    feat: added favicon

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -3,11 +3,8 @@ import React from "react";
 
 export const Navbar = () => {
     return (
-        <div className="w-full text-xl bg-transparent text-white fixed top-0 left-0 h-12 p-10 flex justify-between">
-            <h1>ClipIt</h1>
-            {/* <div className="flex gap-4"> */}
-            {/*     <Button variant={'ghost'}>Sign In</Button> */}
-            {/* </div> */}
+        <div className="w-full text-xl bg-transparent text-white fixed top-0 left-0 md:h-12 h-8 flex justify-between">
+            <img src="/logo.png" alt="ClipIt Logo" className="size-24 md:size-40" />
         </div>
     );
 };
